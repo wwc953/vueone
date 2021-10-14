b6182e040193500205830d69897e52f3266296f2
wangwc <wwc953@163.com>
2021-10-14 17:53:06 +0800

    code

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,5 +6,6 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // baseWebsocketUrl = '“ws://127.0.0.1:7777/websocket/”',
   // 分布式 网关接口
+
   baseWebsocketUrl: '"ws://127.0.0.1:7070/app-websocket/websocket/"'
 })
--- src/utils/websocket.js
+++ src/utils/websocket.js
@@ -36,6 +36,7 @@ vwebsocket.createWebcosket = (socketUrl, heartCheck, timeout, failCount) => {
                     console.log("重试次数", self.fail)
                 }
                 if (self.fail >= (failCount ? failCount : 3)) {
+                    // wbsocket.close();
                     // console.log("重新创建连接,socketUrl:", socketUrl)
                     wbsocket = new WebSocket(socketUrl);
                     // wbsocket.onopen()
@@ -57,7 +58,7 @@ vwebsocket.createWebcosket = (socketUrl, heartCheck, timeout, failCount) => {
                 //心跳检测
                 heartCheckFun.reset().start();
             }
-            console.log("浏览器WebSocket已打开");
+            console.log("浏览器WebSocket已打开，", wbsocket);
         };
         // 监听socket消息接收
         wbsocket.onmessage = function (msg) {
@@ -74,6 +75,7 @@ vwebsocket.createWebcosket = (socketUrl, heartCheck, timeout, failCount) => {
         };
         // 监听socket错误
         wbsocket.onerror = function (e) {
+            console.log(onerror, e)
             Notification({
                 title: "错误",
                 message: "服务器错误，无法接收实时报警信息",
@@ -82,8 +84,33 @@ vwebsocket.createWebcosket = (socketUrl, heartCheck, timeout, failCount) => {
             });
         };
         // 监听socket关闭
-        wbsocket.onclose = function () {
-            console.log("WebSocket已关闭");
+        wbsocket.onclose = function (e) {
+            // 1000	CLOSE_NORMAL	正常关闭; 无论为何目的而创建, 该链接都已成功完成任务.
+            // 1001	CLOSE_GOING_AWAY	终端离开, 可能因为服务端错误, 也可能因为浏览器正从打开连接的页面跳转离开.
+            // 1002	CLOSE_PROTOCOL_ERROR	由于协议错误而中断连接.
+            // 1003	CLOSE_UNSUPPORTED	由于接收到不允许的数据类型而断开连接(如仅接收文本数据的终端接收到了二进制数据).
+            // 1004		保留.其意义可能会在未来定义.
+            // 1005	CLOSE_NO_STATUS	保留.表示没有收到预期的状态码.
+            // 1006	CLOSE_ABNORMAL	保留.用于期望收到状态码时连接非正常关闭(也就是说, 没有发送关闭帧).
+            // 1007	Unsupported Data	由于收到了格式不符的数据而断开连接(如文本消息中包含了非 UTF - 8 数据).
+            // 1008	Policy Violation	由于收到不符合约定的数据而断开连接.这是一个通用状态码, 用于不适合使用 1003 和 1009 状态码的场景.
+            // 1009	CLOSE_TOO_LARGE	由于收到过大的数据帧而断开连接.
+            // 1010	Missing Extension	客户端期望服务器商定一个或多个拓展, 但服务器没有处理, 因此客户端断开连接.
+            // 1011	Internal Error	客户端由于遇到没有预料的情况阻止其完成请求, 因此服务端断开连接.
+            // 1012	Service Restart	服务器由于重启而断开连接.
+            // 1013	Try Again Later	服务器由于临时原因断开连接, 如服务器过载因此断开一部分客户端连接.
+            // 1014		由 WebSocket标准保留以便未来使用.
+            // 1015	TLS Handshake	保留.表示连接由于无法完成 TLS 握手而关闭(例如无法验证服务器证书).
+
+
+            //reason 表示服务器关闭连接的原因. 这是由服务器和子协议决定的.
+            // wasClean 表示连接是否完全关闭.
+            console.log('websocket关闭 =====》 code: ' + e.code + '，reason: ' + e.reason + '，wasClean: ' + e.wasClean)
+            // console.log('websocket关闭 e: ', e)
+            // if (e.code == 1006 || e.code == 1002) {
+            //     console.log("关闭心跳检测")
+            //     heartCheckFun.reset()
+            // }
         };
     }
 
