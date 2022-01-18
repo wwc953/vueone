1a629379d783cf48661facf83f3ae6e596fce6a7
wangwc <wwc953@163.com>
2022-01-18 20:36:08 +0800

    123

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,6 +6,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // baseWebsocketUrl = '“ws://127.0.0.1:7777/websocket/”',
   // 分布式 网关接口
+  // 12312321
 
   baseWebsocketUrl: '"ws://127.0.0.1:7070/app-websocket/websocket/"'
 })
